a073fb6c8d1e7d6444a7ec0760f5acc945bfd32e
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2026-01-14 16:25:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Frog Jump/frog-jump.java
+++ Difficulty: Medium/Frog Jump/frog-jump.java
@@ -1,19 +1,18 @@
-// User function Template for Java
 class Solution {
-    int helper(int[] a, int n, int[] dp) {
+    int[] dp;
+    int helper(int[] a, int n) {
         if(n == 0) return 0;
         if(dp[n] != -1) return dp[n];
-        int left = helper(a, n-1, dp) + Math.abs(a[n] - a[n-1]);
-        int right = Integer.MAX_VALUE;
-        if(n > 1) {
-            right = helper(a, n-2, dp) + Math.abs(a[n] - a[n-2]);
-        }
-        return dp[n] = Math.min(left, right);
+        int next = helper(a, n-1) + Math.abs(a[n] - a[n-1]);
+        int nextNext = Integer.MAX_VALUE;
+        if(n > 1) nextNext = helper(a, n-2) + Math.abs(a[n] - a[n-2]);
+        return dp[n] = Math.min(next, nextNext);
     }
     int minCost(int[] height) {
         // code here
-        int dp[] = new int[height.length+1];
+        int n = height.length;
+        dp = new int[n+1];
         Arrays.fill(dp, -1);
-        return helper(height, height.length-1, dp);
+        return helper(height, n-1);
     }
 }
