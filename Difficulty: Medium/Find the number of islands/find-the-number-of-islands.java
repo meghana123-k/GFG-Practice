6a3ccd8afcc4ed750c58fc16418737cfad664b45
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2026-02-08 00:25:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
+++ Difficulty: Medium/Find the number of islands/find-the-number-of-islands.java
@@ -1,47 +1,31 @@
 class Solution {
     boolean[][] vis;
     int[] dx = {-1, -1, 0, 1, 1, 1, 0, -1};
-    int[] dy = {0, 1,1,1,0, -1, -1, -1};
-    public void dfs(char[][] grid, int i, int j, int n, int m) {
-        if(i < 0 || i >= n || j < 0 || j >= m) return;
-        if(grid[i][j] != 'L' || vis[i][j]) return;
+    int[] dy = {0, 1, 1, 1, 0, -1, -1, -1};
+    public void dfs(char[][] grid, int i, int j) {
+        if(i < 0 || j < 0 || i >= grid.length ||j >= grid[0].length) return;
+        if(vis[i][j] || grid[i][j] != 'L') {
+            return ;
+        }
         if(grid[i][j] == 'L' && !vis[i][j]) {
             vis[i][j] = true;
         }
-        for(int k = 0; k < 8; k++) {
-            dfs(grid, i+dx[k], j+dy[k], n, m);
+        for(int d = 0; d < 8; d++) {
+            dfs(grid, i + dx[d], j + dy[d]);
         }
-        // dfs(grid, i-1 , j, n, m);
-        // dfs(grid, i-1, j+1, n, m);
-        // dfs(grid, i, j+1, n, m);
-        // dfs(grid, i+1, j+1, n, m);
-        // dfs(grid, i+1, j, n, m);
-        // dfs(grid, i+1, j-1, n, m);
-        // dfs(grid, i, j-1, n, m);
-        // dfs(grid, i-1, j-1, n, m);
-        
     }
     public int countIslands(char[][] grid) {
         // Code here
-        int n = grid.length;
-        int m = grid[0].length;
-         vis = new boolean[n][m];
-        int cnt = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
+        int ans = 0;
+        vis = new boolean[grid.length][grid[0].length];
+        for(int i = 0; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length; j++) {
                 if(grid[i][j] == 'L' && !vis[i][j]) {
-                    cnt++;
-                    // vis[i][j] = true;
-                    dfs(grid, i, j, n, m);
+                    ans++;
+                    dfs(grid, i, j);
                 }
             }
         }
-        // for(int i = 0; i < n; i++) {
-        //     for(int j = 0; j < m; j++) {
-        //         System.out.print(vis[i][j]+" ");
-        //     }
-        //     System.out.println();
-        // }
-        return cnt;
+        return ans;
     }
 }
