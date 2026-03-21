ce7d4243d362f4be5a9f5a252e32794220266a46
Meghana Kothakonda <150936095+meghana123-k@users.noreply.github.com>
2026-03-21 23:24:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Activity Selection/activity-selection.java
+++ Difficulty: Medium/Activity Selection/activity-selection.java
@@ -1,24 +1,25 @@
-class Solution {
-    class Pair {
-        int x;
-        int y;
-        Pair(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
+class A {
+    int x, y;
+    A(int x, int y) {
+        this.x = x;
+        this.y = y;
     }
-    public int activitySelection(int[] start, int[] end) {
-         Pair arr[] = new Pair[start.length];
+}
+class Solution {
+    
+    public int activitySelection(int[] start, int[] finish) {
+        // code here
+        A act[] = new A[start.length];
         for(int i = 0; i < start.length; i++) {
-            arr[i] = new Pair(start[i], end[i]);
+            act[i] = new A(start[i], finish[i]);
         }
-        Arrays.sort(arr, (p1, p2) -> Integer.compare(p1.y, p2.y));
+        Arrays.sort(act, (p1, p2) ->Integer.compare(p1.y, p2.y));
+        int fin = act[0].y;
         int cnt = 1;
-        int fin = arr[0].y;
         for(int i = 1; i < start.length; i++) {
-            if(arr[i].x > fin) {
+            if(act[i].x > fin) {
                 cnt++;
-                fin = arr[i].y;
+                fin = act[i].y;
             }
         }
         return cnt;
